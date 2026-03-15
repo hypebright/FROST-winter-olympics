b9dbfb81bf56f9064b35e2d0d9b1f47701ecb5fe
Veerle van Leemput <info@hypebright.nl>
2026-03-15 10:19:23 +0100

    show count up animation on tab switch

--- www/frost.js
+++ www/frost.js
@@ -3,23 +3,24 @@
 
 function animateCount(el) {
   var target = +el.dataset.countup;
-  el.removeAttribute('data-countup');
+  el.classList.add('counting');
   var start = performance.now();
   (function frame(now) {
     var t = Math.min((now - start) / 1400, 1);
     el.textContent = Math.round((1 - Math.pow(1 - t, 3)) * target);
     if (t < 1) requestAnimationFrame(frame);
+    else el.classList.remove('counting');
   })(performance.now());
 }
 
 function initCountUps() {
-  document.querySelectorAll('[data-countup]').forEach(function(el) {
+  document.querySelectorAll('[data-countup]:not(.counting)').forEach(function(el) {
     new IntersectionObserver(function(entries, obs) {
       if (entries[0].isIntersecting) { animateCount(el); obs.disconnect(); }
     }, { threshold: 0.3 }).observe(el);
   });
 }
 
-$(document).on('shiny:connected shiny:value', function() {
+$(document).on('shiny:connected shiny:value shown.bs.tab', function() {
   setTimeout(initCountUps, 150);
 });
